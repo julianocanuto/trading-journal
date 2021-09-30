03d0577e8fcb8f0826fc96c3a95fc19802299213
Juliano Canuto <julianocanuto@gmail.com>
2021-09-30 16:52:01 -0300

    Project setup

---
+++ backend/src/main/java/com/julianocanuto/tradinjournal/TradinjournalApplication.java
@@ -0,0 +1,13 @@
+package com.julianocanuto.tradinjournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradinjournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradinjournalApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/julianocanuto/tradinjournal/TradinjournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julianocanuto.tradinjournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradinjournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
